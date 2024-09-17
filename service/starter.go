2313cfb672ccf599d6069834805621e9964ee186
박기현 <kihyun1998@penta.co.kr>
2024-09-17 20:47:40 +0900

    Refactor: 이름 변경

--- service/starter.go
+++ service/starter.go
@@ -9,23 +9,23 @@ import (
 )
 
 func StartPrice(c *gin.Context, serviceDiscoveryURL string) {
-	serviceAddress, err := GetServiceAddress("autro-price", serviceDiscoveryURL)
+	serviceAddress, err := GetServiceAddress("abprice", serviceDiscoveryURL)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to get autro-price address: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to get abprice address: %v", err)})
 		return
 	}
 
 	url := fmt.Sprintf("http://%s/start", serviceAddress)
 	resp, err := http.Post(url, "application/json", nil)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to start autro-price service : %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to start abprice service : %v", err)})
 		return
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to read response from autro-price service: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Failed to read response from abprice service: %v", err)})
 		return
 	}
 
